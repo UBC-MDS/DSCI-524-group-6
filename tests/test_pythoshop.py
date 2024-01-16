cd70428472f6eefc0c41f8bef8265b1da9d2335c
“Rachel <rachel_0619@hotmail.com>
2024-01-15 17:07:15 -0800

    feat: finished the function body for adjust_brightness

--- tests/test_pythoshop.py
+++ tests/test_pythoshop.py
@@ -3,6 +3,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
 from pythoshop.pythoshop import *
+from pythoshop.adjust_brightness import *
 
-adjust_aspect_ratio("mds_logo.png", 100, 100, "crop")
-
+adjust_aspect_ratio("test_img_1.png", 800, 700, "crop")
+adjust_brightness("test_img_1.png", 0.3)
