74a529f516fa94c1acedda89798e5ea19ee78e46
ella-irene <ellaihein@gmail.com>
2024-01-18 14:30:53 -0800

    added one test

--- tests/test_transform_image.py
+++ tests/test_transform_image.py
@@ -27,5 +27,33 @@ class TestResizeImageFunction(unittest.TestCase):
         self.assertEqual(rotated_img.shape[0], 400)
         self.assertEqual(rotated_img.shape[1], 300)
 
+    def test_rotate_horizontal(self):
+        # Create a test image with known dimensions
+        test_image_path = os.path.join(self.temp_dir, 'test_rotate.png')
+        img = np.ones((300, 400, 3), dtype=np.uint8) * 255 
+        plt.imsave(test_image_path, img)
+
+        # Rotate Image
+        transform_image(test_image_path, method = 'flip', direction = 'horizontal')
+
+        # Check if the rotated image has the correct dimensions
+        rotated_img = plt.imread(os.path.join(self.temp_dir, 'test_image_flip_h_img.png'))
+        self.assertEqual(rotated_img.shape[0], 400)
+        self.assertEqual(rotated_img.shape[1], 300)
+
+    def test_rotate_horizontal(self):
+        # Create a test image with known dimensions
+        test_image_path = os.path.join(self.temp_dir, 'test_rotate.png')
+        img = np.ones((300, 400, 3), dtype=np.uint8)
+        plt.imsave(test_image_path, img)
+
+        # Rotate Image
+        transform_image(test_image_path, method = 'flip', direction = 'horizontal')
+
+        # Check if the rotated image has the correct dimensions
+        rotated_img = plt.imread(os.path.join(self.temp_dir, 'test_image_flip_h_img.png'))
+        self.assertEqual(rotated_img.shape[0], 400)
+        self.assertEqual(rotated_img.shape[1], 300)
+
 if __name__ == '__main__':
     unittest.main()
