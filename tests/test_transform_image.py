54c420fa176e5b9e4d024df275a462253b6eb940
ella-irene <ellaihein@gmail.com>
2024-01-18 22:59:31 -0800

    changed file name

--- tests/test_transform_image.py
+++ tests/test_transform_image.py
@@ -1,33 +1,42 @@
+import matplotlib.image as img
+import numpy as np
+from pythoshop import pythoshop
+from pythoshop.transform_image import *
 import unittest
 import os
-import tempfile
-import shutil
-import matplotlib.pyplot as plt
-import numpy as np
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
-from pythoshop.transform_image import *
 
-class TestTransformImageFunction(unittest.TestCase):
+class TestTransformImage(unittest.TestCase):
     def setUp(self):
-        self.temp_dir = tempfile.mkdtemp()
+        self.test_path = "/tests/mds_logo.png"
+        self.test_image = img.imread(os.getcwd() + self.test_path)
+        self.expected_path = "/tests/mds_logo_trns_img.png"
+        self.height, self.width = self.test_image.shape[:2]       
 
-    def tearDown(self):
-        shutil.rmtree(self.temp_dir)
+    def test_rotate(self):
+            transform_image(image_path = self.test_path)
+            try:
+                  created_image = img.imread(os.getcwd() + self.expected_path)
+                  transform_height, transform_width = created_image.shape[:2]
+            except:
+                  raise Exception("Image not saved to expected path.")
 
-    def test_rotate_horizontal(self):
-        # Create a test image with known dimensions
-        test_image_path = os.path.join(self.temp_dir, 'test_rotate.png')
-        img = np.ones((300, 400, 3), dtype=np.uint8) * 255 
-        plt.imsave(test_image_path, img)
+            self.assertEqual(self.height, transform_width)
+            self.assertEqual(self.width, transform_height)
+            
+            os.remove(os.getcwd() + self.expected_path)
+    
+    def test_flip(self):
+            transform_image(image_path = self.test_path, method = "flip", direction = "vertical")
+            try:
+                  created_image = img.imread(os.getcwd() + self.expected_path)
+                  transform_height, transform_width = created_image.shape[:2]
+            except:
+                  raise Exception("Image not saved to expected path.")
 
-        # Rotate Image
-        transform_image(test_image_path, method = 'rotate', direction = 'clockwise')
+            self.assertEqual(self.height, transform_height)
+            self.assertEqual(self.width, transform_width)
 
-        # Check if the rotated image has the correct dimensions
-        rotated_img = plt.imread(os.path.join(self.temp_dir, 'test_image_rotate_h_img.png'))
-        self.assertEqual(rotated_img.shape[0], 400)
-        self.assertEqual(rotated_img.shape[1], 300)
+            os.remove(os.getcwd() + self.expected_path)
 
 if __name__ == '__main__':
     unittest.main()
