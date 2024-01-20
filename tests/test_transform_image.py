c2cb4bd8158fd5bbf28c27b99f0f635d743c36e2
ella-irene <ellaihein@gmail.com>
2024-01-19 16:27:44 -0800

    deleted all instances of unnecessary os

--- tests/test_transform_image.py
+++ tests/test_transform_image.py
@@ -1,38 +1,37 @@
 import matplotlib.image as img
 import numpy as np
-from pythoshop import pythoshop
 from pythoshop.transform_image import *
 import unittest
 import os
 
 class TestTransformImage(unittest.TestCase):
       def setUp(self):
-            self.test_path = "/tests/mds_logo.png"
-            self.test_image = img.imread(os.getcwd() + self.test_path)
-            self.expected_path = "/tests/mds_logo_trns_img.png"
+            self.test_path = "tests/mds_logo.png"
+            self.test_image = img.imread(self.test_path)
+            self.expected_path = "tests/mds_logo_trns_img.png"
             self.height, self.width = self.test_image.shape[:2]
       
       def test_rotate(self):
             transform_image(image_path = self.test_path)
             try:
-                  created_image = img.imread(os.getcwd() + self.expected_path)
+                  created_image = img.imread(self.expected_path)
                   transform_height, transform_width = created_image.shape[:2]
             except:
                   raise Exception("Image not saved to expected path.")
             self.assertEqual(self.height, transform_width)
             self.assertEqual(self.width, transform_height)
-            os.remove(os.getcwd() + self.expected_path)
+            os.remove(self.expected_path)
 
       def test_flip(self):
             transform_image(image_path = self.test_path, method = "flip", direction = "vertical")
             try:
-                  created_image = img.imread(os.getcwd() + self.expected_path)
+                  created_image = img.imread(self.expected_path)
                   transform_height, transform_width = created_image.shape[:2]
             except:
                   raise Exception("Image not saved to expected path.")
             self.assertEqual(self.height, transform_height)
             self.assertEqual(self.width, transform_width)
-            os.remove(os.getcwd() + self.expected_path)
+            os.remove(self.expected_path)
      
       def test_transform_raises_value_error(self):
             with self.assertRaises(ValueError):
