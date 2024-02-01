a0becd588ef9b2d93351a1ccfc097a63b30e84d9
jlee2843 <jenny.jeeun@gmail.com>
2024-01-31 20:03:51 -0800

    added changed function and tests for apply filter

--- src/pythoshop/apply_filter.py
+++ src/pythoshop/apply_filter.py
@@ -3,16 +3,17 @@ import matplotlib.image as mpimg
 import numpy as np
 import os
 
-def apply_filter(image_path, method, degree=0.7):
+def apply_filter(image, method, degree=0.7, verbose=False):
       """
       Apply filter from a chosen range of colours. 
 
-      This function allows a filter to be applied on the image which can be retrieved from 
+      This function allows a filter to be applied on the image which can be retrieved 
+      from ['red', 'blue', 'green', 'gray', 'sepia'].
 
       Parameters
       ----------
-      image_path : str
-            The file path to the image that needs brightness adjustment. The image can be in .jpg or .png format.
+      image_path : image
+            Image object to be worked on. 
       method : str
             Choose from a list of available filters to be applied onto the image. 
       degree : int or float
@@ -41,42 +42,38 @@ def apply_filter(image_path, method, degree=0.7):
       To apply 'aquamarine' filter on 'photo.jpg' by 70%:
       >>> apply_filter('photo.jpg', 'aquamarine', 0.7)
       """
-       
-      try:
-            img = mpimg.imread(image_path)
-      except:
-            raise FileNotFoundError("Your image is not found in the specified directory.")
-
-      new_image = img.copy()
+      new_image = image.copy()
 
       if degree < 0.5:
             raise Exception("Degree of filter shouldn't be smaller than 0.5.")
 
       if method == "sepia":
-            new_image[::, ::, 0] = (img[::, ::, 0] * 0.786 * degree) + (img[::, ::, 1] * 0.769) + (img[::, ::, 2] * 0.189)
-            new_image[::, ::, 1] = (img[::, ::, 0] * 0.349 * degree) + (img[::, ::, 1] * 0.686) + (img[::, ::, 2] * 0.168)
-            new_image[::, ::, 2] = (img[::, ::, 0] * 0.272 * degree) + (img[::, ::, 1] * 0.534) + (img[::, ::, 2] * 0.131)
+            new_image[::, ::, 0] = (image[::, ::, 0] * 0.786 * degree) + (image[::, ::, 1] * 0.769) + (image[::, ::, 2] * 0.189)
+            new_image[::, ::, 1] = (image[::, ::, 0] * 0.349 * degree) + (image[::, ::, 1] * 0.686) + (image[::, ::, 2] * 0.168)
+            new_image[::, ::, 2] = (image[::, ::, 0] * 0.272 * degree) + (image[::, ::, 1] * 0.534) + (image[::, ::, 2] * 0.131)
       elif method == "blue":
-            new_image[:, :, 0] = img[:, :, 0] * (1-degree)
-            new_image[:, :, 1] = img[:, :, 1] * (1-degree)
-            new_image[:, :, 2] = (img[:, :, 2] * (1 * degree if degree is not None else 0))
+            new_image[:, :, 0] = image[:, :, 0] * (1-degree)
+            new_image[:, :, 1] = image[:, :, 1] * (1-degree)
+            new_image[:, :, 2] = (image[:, :, 2] * (1 * degree if degree is not None else 0))
       elif method == "gray":
-            new_image[:, :, 0] = (img[::, ::, 0] * (1-degree)) 
-            new_image[:, :, 1] = (img[::, ::, 1] * (1-degree))
-            new_image[:, :, 2] = (img[::, ::, 2] * (1-degree))
+            new_image[:, :, 0] = (image[::, ::, 0] * (1-degree)) 
+            new_image[:, :, 1] = (image[::, ::, 1] * (1-degree))
+            new_image[:, :, 2] = (image[::, ::, 2] * (1-degree))
       elif method == "red":
-            new_image[:, :, 0] = (img[:, :, 0] * (1 * degree if degree is not None else 0))
-            new_image[:, :, 1] = img[:, :, 1] * (1-degree)
-            new_image[:, :, 2] = img[:, :, 2] * (1-degree)
+            new_image[:, :, 0] = (image[:, :, 0] * (1 * degree if degree is not None else 0))
+            new_image[:, :, 1] = image[:, :, 1] * (1-degree)
+            new_image[:, :, 2] = image[:, :, 2] * (1-degree)
       elif method == "green":
-            new_image[:, :, 0] = img[:, :, 0] * (1-degree)
-            new_image[:, :, 1] = (img[:, :, 1] * (1 * degree if degree is not None else 0))
-            new_image[:, :, 2] = img[:, :, 2] * (1-degree)
+            new_image[:, :, 0] = image[:, :, 0] * (1-degree)
+            new_image[:, :, 1] = (image[:, :, 1] * (1 * degree if degree is not None else 0))
+            new_image[:, :, 2] = image[:, :, 2] * (1-degree)
       else:
             raise ValueError("Method is not an accepted type.")
 
       new_image = np.clip(new_image, 0, 1)
-      plt.imshow(new_image)
-      plt.show()
 
-      mpimg.imsave(f"{image_path}_filter_img.png", new_image)
+      if verbose:
+            plt.imshow(new_image)
+            plt.show()
+      
+      return new_image
--- tests/test_apply_filter.py
+++ tests/test_apply_filter.py
@@ -11,40 +11,52 @@ def test_data():
             test_image = "tests/test_img_1.png"
             wrong_image = "tests/test_img_1_non.png"
             test_degree = 0.6
-            test_method = "red"
+            tolerance = 0.5
+            test_image_opened = mpimg.imread(test_image)
       return TestData
 
 @pytest.mark.usefixtures("test_data")
 class TestApplyFilter:
-      def test_apply_filter(self, test_data):
-
-            test_image_opened = mpimg.imread(test_data.test_image)
-
-            expected_image_path = f"{test_data.test_image}_filter_img.png"
-
-            apply_filter(image_path=test_data.test_image, method=test_data.test_method, degree=test_data.test_degree)
-
-            try:
-                  created_image = mpimg.imread(expected_image_path)
-            except:
-                  raise Exception("Filter applied image is not saved at the right path.")
-
-            tolerance = 0.5
+      def test_apply_filter_red(self, test_data):
             
-            assert np.allclose(created_image[:,:,0], (test_image_opened[:, :, 0] * (1-test_data.test_degree)), atol=tolerance)
-            assert np.allclose(created_image[:,:,1], (test_image_opened[:, :, 1] * (1-test_data.test_degree)), atol=tolerance)
-            assert np.allclose(created_image[:,:,2], (test_image_opened[:, :, 2] * test_data.test_degree), atol=tolerance)
+            created_image = apply_filter(image=test_data.test_image_opened, method="red", degree=test_data.test_degree)
+            
+            assert np.allclose(created_image[:,:,0], (test_data.test_image_opened[:, :, 0] * test_data.test_degree), atol=test_data.tolerance)
+            assert np.allclose(created_image[:,:,1], (test_data.test_image_opened[:, :, 1] * (1-test_data.test_degree)), atol=test_data.tolerance)
+            assert np.allclose(created_image[:,:,2], (test_data.test_image_opened[:, :, 2] * (1-test_data.test_degree)), atol=test_data.tolerance)
 
-            os.remove(expected_image_path)
+      def test_apply_filter_blue(self, test_data):
+            created_image = apply_filter(image=test_data.test_image_opened, method="blue", degree=test_data.test_degree)
+            
+            assert np.allclose(created_image[:,:,0], (test_data.test_image_opened[:, :, 0] * (1-test_data.test_degree)), atol=test_data.tolerance)
+            assert np.allclose(created_image[:,:,1], (test_data.test_image_opened[:, :, 1] * (1-test_data.test_degree)), atol=test_data.tolerance)
+            assert np.allclose(created_image[:,:,2], (test_data.test_image_opened[:, :, 2] * test_data.test_degree), atol=test_data.tolerance)
+
+      def test_apply_filter_green(self, test_data):
+            created_image = apply_filter(image=test_data.test_image_opened, method="green", degree=test_data.test_degree)
+            
+            assert np.allclose(created_image[:,:,0], (test_data.test_image_opened[:, :, 0] * (1-test_data.test_degree)), atol=test_data.tolerance)
+            assert np.allclose(created_image[:,:,1], (test_data.test_image_opened[:, :, 1] * test_data.test_degree), atol=test_data.tolerance)
+            assert np.allclose(created_image[:,:,2], (test_data.test_image_opened[:, :, 2] * (1-test_data.test_degree)), atol=test_data.tolerance)
+
+      def test_apply_filter_sepia(self, test_data):
+            created_image = apply_filter(image=test_data.test_image_opened, method="sepia", degree=test_data.test_degree)
+            
+            assert np.allclose(created_image[::,::,0], ((test_data.test_image_opened[::, ::, 0] * 0.786 * test_data.test_degree) + (test_data.test_image_opened[::, ::, 1] * 0.769) + (test_data.test_image_opened[::, ::, 2] * 0.189)), atol=test_data.tolerance)
+            assert np.allclose(created_image[::,::,1], ((test_data.test_image_opened[::, ::, 0] * 0.349 * test_data.test_degree) + (test_data.test_image_opened[::, ::, 1] * 0.686) + (test_data.test_image_opened[::, ::, 2] * 0.168)), atol=test_data.tolerance)
+            assert np.allclose(created_image[::,::,2], ((test_data.test_image_opened[::, ::, 0] * 0.272 * test_data.test_degree) + (test_data.test_image_opened[::, ::, 1] * 0.534) + (test_data.test_image_opened[::, ::, 2] * 0.131)), atol=test_data.tolerance)
+
+      def test_apply_filter_gray(self, test_data):
+            created_image = apply_filter(image=test_data.test_image_opened, method="gray", degree=test_data.test_degree)
+            
+            assert np.allclose(created_image[:,:,0], (test_data.test_image_opened[:, :, 0] * (1-test_data.test_degree)), atol=test_data.tolerance)
+            assert np.allclose(created_image[:,:,1], (test_data.test_image_opened[:, :, 1] * (1-test_data.test_degree)), atol=test_data.tolerance)
+            assert np.allclose(created_image[:,:,2], (test_data.test_image_opened[:, :, 2] * (1-test_data.test_degree)), atol=test_data.tolerance)
 
       def test_apply_filter_raise_exception(self, test_data):
             with pytest.raises(Exception, match="Degree of filter shouldn't be smaller than 0.5."):
-                  apply_filter(test_data.test_image, "green", 0.3)
+                  apply_filter(test_data.test_image_opened, "green", 0.3)
 
       def test_apply_filter_raise_valueerror(self, test_data):
             with pytest.raises(ValueError, match="Method is not an accepted type."):
-                  apply_filter(test_data.test_image, "violet")
-
-      def test_apply_filter_raise_filenotfounderror(self, test_data):
-            with pytest.raises(FileNotFoundError, match="Your image is not found in the specified directory."):
-                  apply_filter(test_data.wrong_image, "violet")
+                  apply_filter(test_data.test_image_opened, "violet")
